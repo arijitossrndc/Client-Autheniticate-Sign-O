a0b51408b2d6118c739cf2b049ae3209fdae42bd
sagar <mittalsagar163@gmail.com>
2019-08-17 00:06:52 +0530

    initial commit

--- visitor/admin.py
+++ visitor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from visitor.models import UserRegistration, Departments,Employee,Visitors
+admin.site.register(UserRegistration)
+admin.site.register(Departments)
+admin.site.register(Employee)
+admin.site.register(Visitors)
